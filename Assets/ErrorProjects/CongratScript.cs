a5b8fbf3b56ae0216bd69d41507bfe25af2bd207
Aaron Huggins <90935816+MaelstromDef@users.noreply.github.com>
2023-07-06 16:30:38 -0700

    Initial Commit
    Initial Commit with base Unity Project

---
+++ Assets/ErrorProjects/CongratScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh Text;
+    public ParticleSystem SparksParticles;
+
+    private List<string> TextToDisplay = new List<string>();
+
+    public float RotatingSpeed;
+    public float TextTime;
+    private float TimeToNextText;
+
+    private int CurrentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeToNextText = 0.0f;
+        CurrentText = 0;
+
+        TextToDisplay.Add("Congratulations");
+        TextToDisplay.Add("All Errors Fixed");
+
+        Text.text = TextToDisplay[CurrentText];
+
+        SparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.transform.Rotate(0f, Time.deltaTime * RotatingSpeed, 0f);  // Rotate text
+        TimeToNextText += Time.deltaTime;
+
+        // Wooo next text!!
+        if (TimeToNextText > TextTime)
+        {
+            TimeToNextText = 0.0f;
+            CurrentText++;
+            
+            if (CurrentText >= TextToDisplay.Count) CurrentText = 0;    // Maintain Boundaries
+
+            Text.text = TextToDisplay[CurrentText];                     // Don't forget next text doofus
+        }
+    }
+}
---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [Header("Visuals")]
+    public MeshRenderer Renderer;
+    public Color color;
+    [Header("Rotate")]
+    public float speed = 1f;
+    [Header("Oscillate")]
+    public float distance;
+    public float time;
+    float stopwatch = 0f;
+    int direction = 1;
+
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = color;
+    }
+    
+    void Update()
+    {
+        stopwatch += Time.deltaTime;
+
+        if (stopwatch > time)
+        {
+            stopwatch = 0;
+            direction *= -1;
+        }
+
+        transform.Rotate(10.0f * Time.deltaTime * speed, 0.0f, 0.0f);
+        float newPos = Mathf.Lerp((4 + distance * direction * -1), 4 + distance * direction, stopwatch / time);
+        transform.position = new Vector3(3, newPos, 1);
+    }
+}
